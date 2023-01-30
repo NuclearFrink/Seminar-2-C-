75052590e2ab73429aa2ae8d7bc83db172c63e94
Grigoriy <kutsaevg@yandex.ru>
2023-01-30 21:57:01 +0300

    решали задачи на втором семинаре

---
+++ Example002/Program.cs
@@ -0,0 +1,15 @@
+﻿// найти координаты x y
+int numX = 0;
+int numY = 0;
+
+Console.WriteLine("Задайте X,  ");
+numX = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Задайте Y,  ");
+numY = Convert.ToInt32(Console.ReadLine());
+
+
+if ((numX > 0) && (numY > 0)) Console.WriteLine("Точка в плоскости I ");
+if ((numX < 0) && (numY > 0)) Console.WriteLine("Точка в плоскости II ");
+if ((numX < 0) && (numY < 0)) Console.WriteLine("Точка в плоскости III ");
+if ((numX > 0) && (numY < 0)) Console.WriteLine("Точка в плоскости IV ");
